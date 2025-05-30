ac32765e1555aa77e4ad41f86c47ca10db455336
maarten400 <89874001+maarten400@users.noreply.github.com>
2025-05-30 18:22:46 +0200

    Update setup-debian.sh

--- setup-debian.sh
+++ setup-debian.sh
@@ -8,3 +8,8 @@ sudo apt install nano curl git wget -y
 
 echo "ssh generating keys"
 ssh-keygen -t ed25519 
+
+cat ~/.ssh/id_ed25519.pub | cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys
+
+sudo sed -i '/^#\?PasswordAuthentication/c\PasswordAuthentication no' /etc/ssh/sshd_config
+sudo sed -i '/^#\?PubkeyAuthentication/c\PubkeyAuthentication yes' /etc/ssh/sshd_config
