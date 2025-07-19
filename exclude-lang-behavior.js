53f60f6246fb0fb548dbc095c61f181a08edfa49
maarten400 <89874001+maarten400@users.noreply.github.com>
2025-07-19 15:34:05 +0200

    Update exclude-lang-behavior.js

--- exclude-lang-behavior.js
+++ exclude-lang-behavior.js
@@ -1,15 +1,5 @@
 ({
   beforeAddUrl: ({ url }) => {
-    const allowedLangs = ['en', 'nl', 'de'];
-    try {
-      const hostname = new URL(url).hostname;
-      const match = hostname.match(/^([a-z\-]+)\.wikipedia\.org$/);
-      if (match) {
-        return allowedLangs.includes(match[1]);
-      }
-      return true;
-    } catch {
-      return false;
-    }
+    return false;
   }
 })
