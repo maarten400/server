89965541b4fa63884fcb0ac042c78074b98e4a23
maarten400 <89874001+maarten400@users.noreply.github.com>
2026-04-18 17:45:06 +0200

    docker

--- setup-debian.sh
+++ setup-debian.sh
@@ -60,4 +60,33 @@ sudo unattended-upgrade --dry-run --debug
 
 echo "[✓] Unattended upgrades are now set to install ALL updates."
 
+echo installing docker
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+echo docker installed
+
 echo "copy public key from .ssh folder and run sudo systemctl restart ssh"
